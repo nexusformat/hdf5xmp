7fd98e24c01deef1b1b008df77019f9af48af865
scm <schaedeli@semafor.ch>
2016-08-22 13:37:32 +0200

    Removed things in gitignore

--- cppbuild/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/scm/projects/hdf5Thumbnails/cpp
-# Build directory: /home/scm/projects/hdf5Thumbnails/cppbuild
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
