5d75dabd9f183e8cef6ad3b1d5a3c41855a0dacb
scm <schaedeli@semafor.ch>
2016-09-02 09:00:02 +0200

    ignored build folder

--- cpp/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/scm/projects/hdfthumbs/cpp
-# Build directory: /home/scm/projects/hdfthumbs/cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(kde)
-subdirs(gnome)
--- cpp/build/gnome/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/scm/projects/hdfthumbs/cpp/gnome
-# Build directory: /home/scm/projects/hdfthumbs/cpp/build/gnome
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
